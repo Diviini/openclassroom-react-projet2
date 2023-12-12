68e2c511b1dbab6d76849e295bd38a54ce227cb1
Divini <92796539+Diviini@users.noreply.github.com>
2023-12-12 15:54:09 +0100

    Stylisation des composants

--- shiny-agency/src/pages/Results/index.jsx
+++ shiny-agency/src/pages/Results/index.jsx
@@ -1,9 +1,9 @@
 function Results() {
-    return (
-        <div>
-            <h1>Voila tes résultats !</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Résultats</h1>
+    </div>
+  )
 }
 
 export default Results
